296835c9be56b9499ce47562a2eb645c41efeb20
unknown <you@example.com>
2020-06-08 11:28:47 +0800

    'async+await简化代码'

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -30,43 +30,65 @@ export default {
   },
   methods: {
     // 登录请求
-    handleLogin () {
-      this.$http.post('login', this.formdata).then(res => {
-        const {
-          data,
-          meta: {msg, status}
-        } = res.data
+    async handleLogin () {
+    // 希望  让异步操作的代码 看起来像同步代码
+    // ES7  async+await
+      const res = await this.$http.post('login', this.formdata)
+      const {
+        data,
+        meta: {msg, status}
+      } = res.data
 
-        if (status === 200) {
-          // 登录成功
-          // 1.跳转home
-          this.$router.push({name: 'home'})
-          // 2.提示成功
-          this.$message.success(msg)
-        } else {
-          this.$message.error(msg)
-        }
-      })
+      if (status === 200) {
+      // 登录成功
+      // 1.跳转home
+        this.$router.push({name: 'home'})
+        // 2.提示成功
+        this.$message.success(msg)
+      } else {
+        this.$message.error(msg)
+      }
+
+    // this.$http.post('login', this.formdata).then(res => {
+    //   const {
+    //     data,
+    //     meta: {msg, status}
+    //   } = res.data
+    //
+    //   if (status === 200) {
+    //     // 登录成功
+    //     // 1.跳转home
+    //     this.$router.push({name: 'home'})
+    //     // 2.提示成功
+    //     this.$message.success(msg)
+    //   } else {
+    //     this.$message.error(msg)
+    //   }
+    // })
+    // }
     }
   }
+
 }
 </script>
 
 <style scoped>
-.login-wrap {
-  height: 100%;
-  background-color: #324152;
-  /*弹性布局*/
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.login-wrap .login-form {
-  width: 400px;
-  background-color: #ffffff;
-  border-radius: 5px;
-  padding: 30px;
-}
+  .login-wrap {
+    height: 100%;
+    background-color: #324152;
+    /*弹性布局*/
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .login-wrap .login-form {
+    width: 400px;
+    background-color: #ffffff;
+    border-radius: 5px;
+    padding: 30px;
+  }
+
   .login-wrap .login-btn {
     width: 100%;
   }
