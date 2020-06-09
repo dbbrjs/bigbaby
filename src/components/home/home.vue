62885c0327fd8414756216f87319bc657d77998a
任俊燊 <734496343@qq.com>
2020-06-09 21:57:27 +0800

    退出

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -13,7 +13,7 @@
         </el-col>
         <el-col :span="2">
           <div class="grid-content bg-purple">
-            <a href="#" class="loginout">退出</a>
+            <a href="#" class="loginout" @click="handleSignout">退出</a>
           </div>
         </el-col>
       </el-row>
@@ -116,6 +116,16 @@ export default {
       this.$router.push({name: 'login'})
     }
     // if token 有->继续渲染组件
+  },
+  methods: {
+    handleSignout() {
+      // 1.清除token
+      localStorage.clear()
+      // 2.提示
+      this.$message.success('退出成功')
+      // 3.来到login组件
+      this.$router.push({name: 'login'})
+    }
   }
 }
 </script>
