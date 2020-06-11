fe27c4e2054578e28b9fb4fbbeddcdf17d2d8afb
任俊燊 <734496343@qq.com>
2020-06-11 14:11:22 +0800

    状态和操作

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui'
 import MyHttpServer from './plugins/http'
+import moment from 'moment'
 // 不要忘记引入css文件
 import 'element-ui/lib/theme-chalk/index.css'
 // 导入全局样式
@@ -16,6 +17,11 @@ Vue.use(ElementUI)
 Vue.use(MyHttpServer)
 
 Vue.config.productionTip = false
+
+// 全局过滤器 - 处理日期
+Vue.filter('fmtdata', (v) => {
+  return moment(v).format('YYYY-MM-DD')
+})
 
 /* eslint-disable no-new */
 new Vue({
